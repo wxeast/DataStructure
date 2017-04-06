a654d47efdc67eda48df7a25274e6fd33184bf3b
wxeast <1418657680@qq.com>
2017-04-06 16:53:32 +0800

    wxd:实现的无头的的双向链表 ，，内部含有自己实现的迭代器

--- List.h
+++ List.h
@@ -1,6 +1,8 @@
 #pragma  once 
 #include<assert.h>
-#include"stl_allocator.h"//自己实现的空间配置器     git地址   https://github.com/wxeast/STL
+#include"stl_allocator.h"//自己实现的空间配置器     git地址  https://github.com/wxeast/STL/blob/master/stl_allocator.h
+#include"stl_construct.h"//https://github.com/wxeast/STL/blob/master/stl_construct.h
+#include"stl_iterator.h" //https://github.com/wxeast/STL/blob/master/stl_iterator.h
 //实现的带有头结点的双向链表
 //链表的节点
 template<class T>
@@ -15,96 +17,135 @@ struct  ListNode
 		,_prev(NULL)
 	{}
 };
+
+
+//实现链表的迭代器
+template<class T,class  Ref,class Ptr>
+struct ListIterator
+{
+	typedef ListNode<T>  Node;
+	typedef ListIterator<T,Ref,Ptr> Self;
+	//成员变量
+	Node*  _node;
+	ListIterator(Node*  node =NULL)
+		:_node(node)
+	{}
+
+
+	/////////////////////////////////////////////////////////
+	//迭代器的五种型别    
+	typedef typename InputIteratorTag			IteratorCategory;//迭代器的分类  在这里使用的是一个切片来
+	//既可以用来表示单向迭代器，也可以表示的是双向迭代器
+	typedef T									ValueType;//迭代器的内嵌类型
+	typedef size_t								DifferenceType;//迭代器的距离的最大容量
+	typedef Ptr									Pointer;//迭代器的内嵌指针(迭代器所指指的对象)
+	typedef Ref									Reference;//迭代器的内嵌的内容
+	////////////////////////////////////////////////////////
+
+	Ref  operator*()
+	{
+		return _node->_data;
+	}
+
+	Ptr  operator->()
+	{
+		return  &(operator*());
+	}
+
+	Self& operator++()
+	{
+		_node = _node->_next;
+		return *this;
+	}
+	Self operator++(int)
+	{
+		Node *  node= _node;
+		_node = _node->_next;
+		return  node;
+	}
+
+	bool operator!=(Self&  it)
+	{
+		return  _node!=it._node;
+	}
+};
+
+
+
 //带有头结点的双向链表
 template<class T,class Alloc = alloc>
 class List
 {
 	typedef ListNode<T> Node;
 public:
+	typedef ListIterator<T,T&,T*> Iterator;
+	typedef ListIterator<T,const T&,const T*> ConstIterator;
 	List()
-		:_phead(BuyNode())
+		:_phead(_BuyNode())
 	{
-		new(_phead)Node();
+		//让头节点的    前指针与后指针都指向自己
+		_phead->_next = _phead;
+		_phead->_prev = _phead;
+
 	}
 	
 	void PushBack(const T& data)
 	{
-		Node *  node = BuyNode(data);
-		Node*   tail=  _phead->_prev;//得到的是链表的尾节点
-		if(tail)//要是尾节点存在
-		{
-			tail->_next = node;
-			node->_prev =  tail; 
-			node->_next = _phead;
-		}
-		else//尾节点不存在、、、表示的是 链表的内部没有节点
-		{
-			_phead->_next = node;
-			node->_next = _phead;
-			node->_prev = _phead;
-		}
-		_phead ->_prev = node;	
+		Node *  node = _BuyNode(data);
+		Node *  tail= _phead->_prev;//表示的为节点
+
+		tail->_next =node;
+		node->_prev = tail;
+
+		node->_next = _phead;
+		_phead->_prev =node;
 	}
 	void PopBack()
 	{
-		Node* tail = _phead->_prev;
-		Node* head = _phead->_next;
-		if(tail == NULL)//表示的是当前的链表为空
+		Node* tail = _phead->_prev;//表示的是 当前的尾节点
+		Node* prev  = tail->_prev;//表示的是尾节点前的节点
+
+		//表示的是链表内无节点
+		if(tail == _phead)
+			return;
+		else
 		{
-			return  ;
-		}
-		else if(head == tail)//表示的当前的链表只有这么一个节点
-		{
-			_phead->_next = _phead->_prev =NULL;
-			DeleteNode(tail);
-		}
-		else//表示的是链表不只有一个节点
-		{
-			Node * prev =tail->_prev;
 			prev->_next = _phead;
 			_phead->_prev = prev;
-			DeleteNode(tail);
+			_DestroyNode(tail);
 		}
+	
 	}
 	void PushFront(const T& data)
 	{
-		Node*  node = BuyNode(data);
-		Node* head = _phead->_next;
+		Node* node= _BuyNode(data);
+		Node* head = _phead->_next;//得到的是当前的链表的第一个结点
+
 		_phead->_next = node;
-		node->_prev =_phead; 
-		if(head)
-		{
-			head->_prev  =node;
-			node->_next  =head;
-		}
-		else
-		{
-			node->_next = _phead;
-			_phead->_prev  =node;
-		}
-		
-		
+		node->_prev = _phead;
+
+		node->_next = head;
+		head->_prev = node;
 	}
 	void PopFront()
 	{
-		Node*  head  =_phead->_next;
-		if(head ==NULL)//当前链表无节点
-		{
+		Node*  head  =_phead->_next;//得到当前链表的第一个节点
+		Node*  next  = head->_next;//得到当前链表的第二个节点
+		//表示的是当前的链表内没有一个节点
+		if(head == _phead)
 			return ;
-		}
-		else 
+		else
 		{
-			Node* next =  head->_next;
+			next->_prev = _phead;
 			_phead->_next = next;
-			if(next)
-				next->_prev = _phead;
+			_DestroyNode(head);
 		}
-
+	
 	}
-	Node*  Find(const T& data)
+	Iterator Find(const T& data)
 	{
 		Node* cur  = _phead->_next;
-		while(cur&&cur!=_phead)
+		while(cur!=_phead)
 		{
 			if(cur->_data == data)
 				return cur;
@@ -112,42 +153,55 @@ public:
 		}
 		return NULL;
 	}
-	~List()
+
+
+
+
+	Iterator Begin()
 	{
-		Node* cur  = _phead->_next;
-		while(cur&&cur!=_phead)
-		{
-			Node *del  = cur;
-			cur->_next;
-			DeleteNode(del);
-		}
-		DeleteNode(_phead);
+		return _phead->_next;
 	}
-	void PrintList()
-	{
-		Node*  cur =_phead->_next;
-		while(cur&&cur!=_phead)
-		{
-			cout<<cur->_data<<" ";
-			cur =cur->_next;
-		}
-		cout<<endl;
-	}
-	Node*  GetHead()
+	Iterator End()
 	{
 		return _phead;
 	}
+
+	ConstIterator Begin()const
+	{
+		return _phead->_next;
+	}
+	ConstIterator End()const
+	{
+		return _phead;
+	}
+	~List()
+	{
+		Clear();
+		_DestroyNode(_phead);
+		_phead =NULL;
+	}
+	void Clear()
+	{
+		Node * cur = _phead->_next;
+		while(cur!= _phead)
+		{
+			Node *del =cur;
+			cur =cur->_next;
+			_DestroyNode(del);
+		}
+	}
 protected:
-	Node*  BuyNode(const T&  data =T())
+	Node*  _BuyNode(const T&  data =T())
 	{
 		Node *  node  = data_allocator::allocate();
-		new(node)Node(data);
+		Construct(node,data);
 		return  node;
 	}
-	void DeleteNode(Node* del)
+	void _DestroyNode(Node* del)
 	{
 		if(del)
 		{
+			Destroy(del);
 			data_allocator::deallocate(del);
 		}
 		del = NULL;
@@ -165,7 +219,16 @@ void TestList()
 	l.PushFront(3);
 	l.PushFront(4);
 	l.PushFront(5);
-	l.PrintList();
-	ListNode<int> *  node = l.Find(10);
+	
+
+	List<int>::Iterator it   =l.Begin();
+	while(it != l.End())
+	{
+		cout<<*it<<" ";
+		++it;
+	}
+	cout<<endl;
+	cout<<"List:"<<Distance(l.Begin(),l.End())<<endl;
+
 	
 }
